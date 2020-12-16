452746516929ff204e0076de70d7bd544e3d2665
Oleksii Mykhailetskyi <melsmels1526@gmail.com>
2020-12-16 06:44:20 +0200

    add footer

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 let mobileMenuBtn = document.querySelector(".nav-mobile-menu");
 let mobileMenu = document.querySelector(".nav-header")
-console.log(mobileMenuBtn)
-console.log(mobileMenu)
+let elNav = document.querySelector(".list-header")
+console.log(elNav)
 
 mobileMenuBtn.addEventListener("click", activeMenu)
 
 function activeMenu() {
-
     mobileMenuBtn.classList.toggle("active-menu")
     if (mobileMenuBtn.classList.toggle("active-menu")) {
         mobileMenu.classList.remove("active-menu")
@@ -14,12 +13,17 @@ function activeMenu() {
     if (mobileMenuBtn.classList.toggle("active-menu")) {
         mobileMenu.classList.add("active-menu")
     }
-
 }
+mobileMenu.addEventListener("click",function (e) {
+    if (e.target == mobileMenu || e.target == elNav ) {
+        mobileMenu.classList.remove("active-menu")
+        mobileMenuBtn.classList.remove("active-menu")
+    }
+})
 
 $(".slider-block").slick({
-    dots:true,
-    arrows:true,
-    autoplay:false,
-  
-  })
+    dots: true,
+    arrows: true,
+    autoplay: false,
+
+})
